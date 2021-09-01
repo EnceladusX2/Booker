28c99921a40b48707c84091b7911b9ab36c0b1b9
EnceladusX2 <srift64@outlook.com>
2021-08-31 23:23:48 -0700

    Get basic bot implementation working.

--- BookerBot.cs
+++ BookerBot.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Threading.Tasks;
+
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
 
 namespace Booker
 {
     public class BookerBot
     {
+        // TODO: Redo this to support config files, as well as more complex features.
+        public async Task RunAsync()
+        {
+            //* Create a new DiscordShardedClient, so we can both connect to the bot account, and not have to worry about manually writing any sharding code.
+            var bot = new DiscordShardedClient(new DiscordConfiguration
+            {
+                //! Fix this, the bot token shouldn't be visible in the code.
+                Token = "token",
+                TokenType = TokenType.Bot
+            });
 
+            //* Setup CommandsNext to also work with shards.
+            await bot.UseCommandsNextAsync(new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { "b!" }
+            });
+
+            await bot.StartAsync();
+            await Task.Delay(-1);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,10 +4,11 @@ namespace Booker
 {
     class Program
     {
+        //* Entry point of the bot, as well as where we'll send the needed data for the bot to function.
         static void Main(string[] args)
         {
             var Bot = new BookerBot();
-            // TODO: Setup rest of bot in a RunAsync method, then call it here.
+            Bot.RunAsync().GetAwaiter().GetResult();
         }
     }
 }
